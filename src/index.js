10660c71a86c9c857c1507036c996203953dc8a7
yayoi44 <67957676+yayoi44@users.noreply.github.com>
2020-08-14 18:18:32 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// past
+const traditionalFunction = function() {
+  return "traditional";
+};
+
+const resTraditional = traditionalFunction();
+console.log(resTraditional);
+
+// arrow
+const arrowFunction = () => {
+  return "hello";
+};
+
+const res = arrowFunction();
+console.log(res);
+
+// practice
+const uncle = () => {
+  return "Uncle JavaScript";
+};
+
+const practice = uncle();
+console.log(practice);
